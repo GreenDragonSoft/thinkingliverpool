673558c798cb8ec774c17345eb1f86ea36b2f038
Paul M Furley <paul@paulfurley.com>
2016-11-03 18:49:39 +0000

    Convert remaining URLs to whats-on-in-liverpool

--- thinkingweekly/apps/events/urls.py
+++ thinkingweekly/apps/events/urls.py
@@ -17,15 +17,15 @@ urlpatterns = [
         views.EventList.as_view(),
         name='events.event_list'),
 
-    url(r'^events-in-liverpool/past-events/$',
+    url(r'^whats-on-in-liverpool/past-events/$',
         views.PastUpdatesList.as_view(),
         name='events.past_updates'),
 
-    url(r'^events-in-liverpool/(?P<date>' + DATE_PATTERN + ')/$',
+    url(r'^whats-on-in-liverpool/(?P<date>' + DATE_PATTERN + ')/$',
         views.WeeklyUpdateEventList.as_view(),
         name='events.weekly_update_event_list'),
 
-    url(r'^events-in-liverpool/(?P<slug>.+)/$',
+    url(r'^whats-on-in-liverpool/(?P<slug>.+)/$',
         views.VenueEventList.as_view(),
         name='events.venue_event_list'),
 
